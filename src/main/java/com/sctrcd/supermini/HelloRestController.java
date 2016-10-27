ff71e39e7694f4c954f5ef77237992992f8c4e5d
ketonemaniac <ketonemaniac@gmail.com>
2016-10-28 00:16:29 +0800

    add environment variables

--- src/main/java/com/sctrcd/supermini/HelloRestController.java
+++ src/main/java/com/sctrcd/supermini/HelloRestController.java
@@ -1,9 +1,14 @@
 package com.sctrcd.supermini;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HelloRestController {
@@ -16,4 +21,20 @@ public class HelloRestController {
         return "Hello, World!";
     }
 
+
+    /**
+     * https://spring.io/blog/2011/10/13/using-cloud-foundry-services-with-spring-part-1-the-basics
+     * @param response
+     * @throws IOException
+     */
+    @RequestMapping("/env")
+    public void env(HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("System Environment:");
+        for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
+            out.println(envvar.getKey() + ": " + envvar.getValue());
+        }
+        out.println("hello, " + System.getenv().get("hello"));
+    }
 }
